2ba4649eceb136f697a179860470cada557a9b27
Tolu Joel <toluisjoel@gmail.com>
2023-04-10 21:51:07 +0100

    Automatically create slug for products

--- shop/views.py
+++ shop/views.py
@@ -1,15 +1,22 @@
+from django.utils.text import slugify
+
 from rest_framework import generics
 
 from .models import Product
-from .serializers import ProductListSerializer, ProductDetailSerializer
+from .serializers import ProductSerializer
 
 
 class ProductList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
-    serializer_class = ProductListSerializer
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        name = serializer.validated_data.get('name')
+        slug = slugify(name)
+        return serializer.save(creator=self.request.user, slug=slug)
 
 
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     lookup_field = 'slug'
-    serializer_class = ProductDetailSerializer
+    serializer_class = ProductSerializer
