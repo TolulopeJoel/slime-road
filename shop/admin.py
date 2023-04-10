f71645248d98ff9af24bb41fae5b5c948c11a7cd
Tolu Joel <toluisjoel@gmail.com>
2023-04-10 16:49:25 +0100

    Added Product model

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['creator', 'name', 'price', 'created_at']
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Product, ProductAdmin)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    creator = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='product/image/%Y/%m/%d')
+    # link to the creator's product. This could be anything digital (ex: pdf, video, notion page)
+    link = models.URLField()
+    slug = models.SlugField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-updated_at',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
