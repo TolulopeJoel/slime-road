b306d10a2afc75ee52031f6616e6d249a3b290d9
Tolu Joel <toluisjoel@gmail.com>
2023-04-11 08:17:42 +0100

    Added Order model

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['email', 'product', 'price', 'created_at', 'paid']
+
+admin.site.register(Order, OrderAdmin)
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from shop.models import Product
+
+
+class Order(models.Model):
+    product = models.ForeignKey(
+        Product,
+        null=True,
+        related_name='orders',
+        on_delete=models.SET_NULL
+    )
+    email = models.EmailField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    paid = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
