928df7259080974ae40aee3041f6e8c27a6e936d
Tolu Joel <toluisjoel@gmail.com>
2023-05-08 12:38:04 +0100

    changed refresh token endpoint

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('register/', views.RegisterView.as_view(), name='register'),
-    path('auth/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 
     path('user/products/', views.CreatorProducts.as_view(), name='user-products'),
     path('user/library/', views.UserLibrary.as_view(), name='user-library'),
